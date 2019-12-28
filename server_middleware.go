b52c3a03e3c902a7c730cf6927049f74dea1136d
Kent 'picat' Gruber <kgruber1@emich.edu>
2019-12-27 20:47:21 -0500

    Create server_middleware.go

---
+++ server_middleware.go
@@ -0,0 +1,53 @@
+package web
+
+import (
+	"log"
+	"net/http"
+)
+
+type RouteMiddleware func(http.HandlerFunc) http.HandlerFunc
+
+func MiddlewareLogRequest(logger *log.Logger) RouteMiddleware {
+	return func(h http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			logger.Println(r.Method, r.URL.Path, r.RemoteAddr, r.UserAgent())
+			h(w, r)
+		}
+	}
+}
+
+var DefaultRequestBodySize = int64(1024)
+
+func MiddlewareLimitRequestBody(numBytes int64) RouteMiddleware {
+	return func(h http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			r.Body = http.MaxBytesReader(w, r.Body, numBytes)
+			h(w, r)
+		}
+	}
+}
+
+func MiddlewareHeader(headerKey, headerValue string) RouteMiddleware {
+	return func(h http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Add(headerKey, headerValue)
+			h(w, r)
+		}
+	}
+}
+
+func MiddlewareHSTS() RouteMiddleware {
+	return MiddlewareHeader("Strict-Transport-Security", "max-age=63072000; includeSubDomains")
+}
+
+func MiddlewareContentType(contentType string) RouteMiddleware {
+	return MiddlewareHeader("Content-Type", contentType)
+}
+
+func MiddlewareContentTypeUTF8(contentType string) RouteMiddleware {
+	return MiddlewareHeader("Content-Type", contentType+"; charset=UTF-8")
+}
+
+func MiddlewareContentTypeJSON() RouteMiddleware {
+	return MiddlewareContentTypeUTF8("application/json")
+}
