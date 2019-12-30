0988ea685c005d1071befa07dd829d1aa0a2ec16
Kent 'picat' Gruber <kgruber1@emich.edu>
2019-12-30 17:42:44 -0500

    Update server_test.go

--- server_test.go
+++ server_test.go
@@ -1,6 +1,7 @@
 package web
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -27,4 +28,53 @@ func TestNewServer(t *testing.T) {
 	}
 
 	t.Log(server)
+}
+
+func TestNewServerWithGoogleOauth(t *testing.T) {
+	logger := log.New(os.Stderr, "test-web-server: ", log.LstdFlags)
+
+	authenticator, _ := NewOauth2GoogleAuthenticator(
+		WithRedirectToLoginOnAuthFailure(),
+		WithRedirectOnLogout("/goodbye"),
+	)
+
+	tmpl, _ := template.New("homePage").Parse(`
+		<!DOCTYPE html>
+		<html lang="en">
+		<meta charset="utf-8">
+
+		<body>
+			Hello {{.}}
+
+			<a href="/auth/google/logout">Logout</a>
+		</body>
+	`)
+
+	helloWorld := func(w http.ResponseWriter, r *http.Request) {
+		v, ok := authenticator.ReadSessionValue(w, r, "name")
+		if ok {
+			tmpl.Execute(w, v)
+		}
+	}
+
+	mainRoutes := Routes{
+		"/": helloWorld,
+	}
+
+	server, err := NewServer(
+		WithRoutes(
+			JoinRoutes(
+				AuthenticatedRoutes(authenticator, mainRoutes),
+				authenticator.Routes(),
+			),
+			MiddlewareLogRequest(logger),
+		),
+	)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// go test -v -timeout 30m github.com/picatz/web -run TestNewServerWithGoogleOauth
+	Serve(server, nil, "", "")
 }
